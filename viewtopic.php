eeb183c8f463ea971bfd9e1b844fde59f595ad1d
kamwis003 <93602462+kamwis003@users.noreply.github.com>
2025-12-02 16:31:48 +0100

    Update viewtopic.php

--- viewtopic.php
+++ viewtopic.php
@@ -1,80 +1,119 @@
 <?php
-declare(strict_types=1); // włączenie typowania zmiennych w PHP >=7
-session_start(); // zapewnia dostęp do zmienny sesyjnych w danym pliku
-$tid = $_GET['tid'];
+declare(strict_types=1);
+session_start();
+
+$tid = $_GET['tid'] ?? null;
+if (!$tid) {
+    die("Brak ID wątku.");
+}
+
+// --- PDO CONNECTION (Azure MySQL) ---
+$host     = "tcp:forumewaldowe.database.windows.net,1433";
+$dbname   = "forumewaldowe";
+$username = "htmlentities";
+$password = "Ewald123#";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Błąd połączenia: " . $e->getMessage());
+}
 ?>
+
 <a href="viewforum.php">Powrót</a><br>
 Temat:
 <?php
-$connection = mysqli_connect("localhost", "root", "", "01000928_z7");
-$result1 = mysqli_query($connection, "SELECT * from threads WHERE tid='$tid'");
-$threadresults = mysqli_fetch_array($result1);
-$threadname = $threadresults[1];
-echo $threadname;
+// --- POBRANIE NAZWY WĄTKU ---
+$stmt = $pdo->prepare("SELECT tname FROM threads WHERE tid = ?");
+$stmt->execute([$tid]);
+$thread = $stmt->fetch();
+
+if (!$thread) {
+    die("Wątek nie istnieje.");
+}
+
+echo htmlspecialchars($thread['tname'], ENT_QUOTES);
 ?>
-<br>
+<br><br>
+
+<!-- Formularz dodawania posta -->
 Dodaj posta<br>
 <form action="addpost.php" method="post" enctype="multipart/form-data">
-Wiadomość:<input type="text" name="message"><br>
-Plik:<input type="file" name="fileToUpload" id="fileToUpload"><br>
-<input type='hidden' name='tid' value='<?php echo "$tid";?>'/> 
-<input type="submit" value="Wyślij" name="submit" <?php if (!isset($_SESSION['loggedin']))
-{echo "disabled";}?>>
+    Wiadomość: <input type="text" name="message"><br>
+    Plik: <input type="file" name="fileToUpload"><br>
+
+    <input type="hidden" name="tid" value="<?= htmlspecialchars($tid) ?>">
+
+    <input type="submit" value="Wyślij"
+        <?php if (!isset($_SESSION['loggedin'])) echo "disabled"; ?>>
 </form>
+
+<br><br>
+
 <?php
-$connection = mysqli_connect("localhost", "root", "", "01000928_z7");
-if (!$connection)
-{
-echo " MySQL Connection error." . PHP_EOL;
-echo "Errno: " . mysqli_connect_errno() . PHP_EOL;
-echo "Error: " . mysqli_connect_error() . PHP_EOL;
-exit;
+// --- POBRANIE WIADOMOŚCI W WĄTKU ---
+$stmt = $pdo->prepare("SELECT * FROM messages WHERE tid = ?");
+$stmt->execute([$tid]);
+
+echo "<table cellpadding='5' border='1'>";
+echo "<tr>
+        <td>messageid</td>
+        <td>username</td>
+        <td>wiadomość</td>
+        <td>Plik</td>
+        <td>czas</td>
+      </tr>";
+
+while ($row = $stmt->fetch()) {
+
+    // pobranie nazwy użytkownika
+    $userStmt = $pdo->prepare("SELECT username FROM users WHERE id = ?");
+    $userStmt->execute([$row['id']]);
+    $user = $userStmt->fetch();
+
+    $username = $user['username'] ?? '???';
+
+    // filtrowanie przekleństw
+    $message = preg_replace("/\bcholera\b/i", "co przeklinasz?", $row['message']);
+
+    $filename = $row['file'];
+    $datetime = $row['datetime'];
+    $type     = $row['ext'];
+
+    echo "<tr>";
+    echo "<td>{$row['messageid']}</td>";
+    echo "<td>$username</td>";
+    echo "<td>$message</td>";
+
+    echo "<td>";
+
+    // wyświetlanie plików
+    if ($type == "img") {
+        echo "<img src='./files/$filename' width='200' height='200'>";
+    }
+    elseif ($type == "audio") {
+        echo "<audio controls><source src='./files/$filename' type='audio/mp3'></audio>";
+    }
+    elseif ($type == "video") {
+        echo "<video width='320' height='240' controls>
+                <source src='./files/$filename' type='video/mp4'>
+              </video>";
+    }
+
+    echo "</td>";
+
+    echo "<td>$datetime</td>";
+
+    echo "</tr>";
 }
-$tid = $_GET['tid'];
-$result = mysqli_query($connection, "SELECT * from messages WHERE tid='$tid'");
-print "<TABLE CELLPADDING=5 BORDER=1>";
-print "<TR><TD>messageid</TD><TD>username</TD><TD>wiadomość</TD><TD>Plik</TD><TD>czas</TD></TR>\n";
-while ($row = mysqli_fetch_array ($result))
-{
-$messageid = $row[0];
-$userid = $row[2];
-$result2 = mysqli_query($connection, "SELECT * from users WHERE id='$userid'");
-$result2fetch = mysqli_fetch_array($result2);
-$username = $result2fetch[1]; 
-$message = $row[3];
-$message = preg_replace("/\bcholera\b/i", "co przeklinasz?", $message);
-$filename= $row[4];
-$datetime= $row[5];
-print "<TR><TD>$messageid</TD>";
-print "<TD>$username</TD>";
-print "<TD>$message</TD>";
-if($row[6] == "img")
-{
-echo "<td>";?><img src="<?php echo "./files/" . $filename;?>" height="200" width="200"><?php echo "</td>";
-}
-else if($row[6] == "audio")
-{
-echo "<td>";?><audio controls><source src="<?php echo "./files/" . $filename;?>" type="audio/mp3"><?php echo "</td>";    
-}
-else if($row[6] == "video")
-{
-echo "<td>";?><video width="320" height="240" controls><source src="<?php echo "./files/" . $filename;?>" type="video/mp4"><?php echo "</td>";
-}
-else
-{
-    echo "<td></td>";
-}
-print "<TD>$datetime</TD>";
-//if($_SESSION['user_login'] == $username)
-//{
-//    print "<TD>test</TD>";
-//}
-//else 
-//{
-//    print "<TD></TD>";
-//}
-print "</TR>\n";
-}
-print "</TABLE>";
-mysqli_close($connection);
+
+echo "</table>";
 ?>
