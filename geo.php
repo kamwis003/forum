76df460c06678ac720b344a720ec59c4166d034d
kamwis003 <93602462+kamwis003@users.noreply.github.com>
2025-12-02 16:26:24 +0100

    Update geo.php

--- geo.php
+++ geo.php
@@ -1,37 +1,70 @@
-<?php declare(strict_types=1); // włączenie typowania zmiennych w PHP >=7
-$link = mysqli_connect("localhost", "root", "", "01000928_z7");
+<?php declare(strict_types=1);
+
+// --- DANE DO PDO (Azure MySQL) ---
+$host     = "tcp:forumewaldowe.database.windows.net,1433";   // <-- Twój host z Azure
+$dbname   = "forumewaldowe";                            // <-- baza
+$username = "htmlentities";                             // <-- login
+$password = "Ewald123#";                                // <-- hasło
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Błąd połączenia: " . $e->getMessage());
+}
+
+// --- POBRANIE IP UŻYTKOWNIKA ---
 $ipaddress = $_SERVER["REMOTE_ADDR"];
-function ip_details($ip) {
-$json = file_get_contents ("http://ipinfo.io/{$ip}/geo");
-$details = json_decode ($json);
-return $details;
+
+// Funkcja pobierająca geo-info
+function ip_details(string $ip) {
+    $json = @file_get_contents("http://ipinfo.io/{$ip}/geo");
+    return $json ? json_decode($json) : null;
 }
+
 $details = ip_details($ipaddress);
-$ipgoscia =  $details -> ip;
-$sql = mysqli_query($link,"INSERT INTO goscieportalu (ipaddress) VALUES ('$ipgoscia')");
-$tabela =  mysqli_query($link,"SELECT * FROM goscieportalu");
-$linki = "https://www.google.pl/maps/place/";
-$linki .= $ipgoscia;
-$row = mysqli_fetch_assoc($tabela);
+$ipgoscia = $details->ip ?? $ipaddress;
+
+// --- ZAPIS IP DO BAZY ---
+$stmt = $pdo->prepare("INSERT INTO goscieportalu (ipaddress) VALUES (?)");
+$stmt->execute([$ipgoscia]);
+
+// --- POBRANIE LISTY WSZYSTKICH GOŚCI ---
+$tabela = $pdo->query("SELECT * FROM goscieportalu ORDER BY datetime DESC");
+
 ?>
-<table border ="1" cellspacing="0" cellpadding="10">
+<table border="1" cellspacing="0" cellpadding="10">
     <tr>
-    <th>ID</th>
-    <th>Adres IP</th>
-    <th>Czas</th>
-    <th>Lokalizacja</th>
-</tr>
-<?php
-while($row = mysqli_fetch_assoc($tabela)){
+        <th>ID</th>
+        <th>Adres IP</th>
+        <th>Czas</th>
+        <th>Lokalizacja</th>
+    </tr>
+
+<?php foreach ($tabela as $row): ?>
+
+    <?php
+        // indywidualny link do lokalizacji
+        $linki = "https://www.google.pl/maps/place/" . $row['ipaddress'];
     ?>
+
     <tr>
-   <td><?php echo $row['id']; ?> </td>
-   <td><?php echo $row['ipaddress']; ?> </td>
-   <td><?php echo $row['datetime']; ?> </td>
-   <td><a  href="<?= $linki ?>" >Link</a></td>
- <tr>
- <?php
-}
-mysqli_close($link);
-?>
-<a href = "viewforum.php">Do portalu</a>
+        <td><?= htmlspecialchars($row['id']) ?></td>
+        <td><?= htmlspecialchars($row['ipaddress']) ?></td>
+        <td><?= htmlspecialchars($row['datetime']) ?></td>
+        <td><a href="<?= $linki ?>" target="_blank">Link</a></td>
+    </tr>
+
+<?php endforeach; ?>
+
+</table>
+
+<br>
+<a href="viewforum.php">Do portalu</a>
