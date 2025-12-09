56f7ae7665625e9b93527140569eb00049ec7ff1
kacwac000 <kacwac000@pbs.edu.pl>
2025-12-10 00:55:17 +0100

    Update viewforum.php

--- viewforum.php
+++ viewforum.php
@@ -49,8 +49,7 @@ try {
 
 // --- Odczyt threads ---
 $stmt = $pdo->query("SELECT tid, tname FROM threads ORDER BY tid DESC");
-var_dump($stmt->rowCount());
-while ($row = $stmt->fetch()) {
+foreach ($rows as $row) {
     echo "<a href='viewtopic.php?tid=" . htmlspecialchars($row['tid']) . "'>" 
          . htmlspecialchars($row['tname']) . "</a><br>";
 }
