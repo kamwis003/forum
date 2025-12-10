ec885b23bf305a971a129e30826ab1d99a34d365
kacwac000 <kacwac000@pbs.edu.pl>
2025-12-10 19:29:52 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@
 session_start();    // rozpoczęcie sesji
 session_unset();    // usunięcie wszystkich zmiennych sesyjnych
 session_destroy();  // zniszczenie sesji
+setcookie("user_login", "", time() - 3600);
 
 // Przekierowanie do strony logowania
 header("Location: https://forumewaldowe.azurewebsites.net/login.php");
